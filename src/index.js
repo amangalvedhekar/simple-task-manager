2b4ae478065f6ae47c86eae02cb9ef5ebf0f82d3
Anirudh <amangalvedhekar@icloud.com>
2018-07-12 16:30:49 -0400

    redux thunk working sampl

--- src/index.js
+++ src/index.js
@@ -3,20 +3,34 @@ import ReactDOM from 'react-dom';
 import {createStore, applyMiddleware} from 'redux';
 import {Provider} from 'react-redux';
 import thunk from 'redux-thunk';
-import { composeWithDevTools } from 'redux-devtools-extension';
+import {composeWithDevTools} from 'redux-devtools-extension';
 import './index.css';
-import tasks from './reducers';
+import taskReducer from './reducers';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+const rootReducer = (state = {}, action) => {
+  return {
+    tasks: taskReducer(
+      state.tasks,
+      action
+    ),
+  };
+};
+
 const store = createStore(
-  tasks,
-  composeWithDevTools(applyMiddleware(thunk)),
+  rootReducer,
+  composeWithDevTools(
+    applyMiddleware(
+      thunk
+    )
+  ),
 );
 
-ReactDOM.render(<Provider
+ReactDOM.render(
+  <Provider
     store={store}>
-    <App />
+    <App/>
   </Provider>,
   document.getElementById('root'),
 );
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,45 +1,53 @@
-import { uniqueId } from '../actions';
+const initialState = {
+  tasks: [],
+  isLoading: false,
+  error: null,
+};
 
-// const mockTasks = [
-//   {
-//     id: uniqueId(),
-//     title: 'Learn Redux',
-//     description: 'The store, actions, and reducers, oh my!',
-//     status: 'Unstarted',
-//   },
-//   {
-//     id: uniqueId(),
-//     title: 'Peace on Earth',
-//     description: 'No big deal.',
-//     status: 'In Progress',
-//   },
-//   {
-//     id: uniqueId(),
-//     title: 'Foo',
-//     description: 'Bar',
-//     status: 'Completed',
-//   },
-// ];
+export default function tasks(state = initialState, action) {
+  switch (action.type) {
+    case 'FETCH_TASKS_STARTED':
+      return {
+        ...state,
+        isLoading: true,
+      };
 
-export default function tasks(state = { tasks: [] }, action) {
-  if (action.type === 'CREATE_TASK') {
-    return {
-      tasks: state.tasks.concat(action.payload),
-    };
-  }
+    case 'FETCH_TASKS_SUCCEEDED':
+      return {
+        ...state,
+        tasks: action.payload.tasks,
+        isLoading: false,
+      };
 
-  if (action.type === 'EDIT_TASK') {
-    const { payload } = action;
-    return {
-      tasks: state.tasks.map(task => {
-        if (task.id === payload.id) {
-          return Object.assign({}, task, payload.params);
+    case 'FETCH_TASKS_FAILED':
+      return {
+        ...state,
+        isLoading: false,
+        error: action.payload.error,
+      };
+
+    case 'CREATE_TASK_SUCCEEDED':
+      return {
+        ...state,
+        tasks: state.tasks.concat(action.payload.task),
+      };
+
+    case 'EDIT_TASK_SUCCEEDED':
+      const { payload } = action;
+      const nextTasks = state.tasks.map(task => {
+        if (task.id === payload.task.id) {
+          return payload.task;
         }
 
         return task;
-      }),
-    };
-  }
+      });
+      return {
+        ...state,
+        tasks: nextTasks,
+      };
 
-  return state;
+    default: {
+      return state;
+    }
+  }
 }
