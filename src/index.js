9b247e1afa7a164ca38a725f1863d7fbeb94e212
Anirudh <amangalvedhekar@icloud.com>
2018-07-12 15:55:34 -0400

    setting up for redux-thunk

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import {Provider} from 'react-redux';
-import { devToolsEnhancer } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import './index.css';
 import tasks from './reducers';
 import App from './App';
@@ -10,7 +11,7 @@ import registerServiceWorker from './registerServiceWorker';
 
 const store = createStore(
   tasks,
-  devToolsEnhancer(),
+  composeWithDevTools(applyMiddleware(thunk)),
 );
 
 ReactDOM.render(<Provider
